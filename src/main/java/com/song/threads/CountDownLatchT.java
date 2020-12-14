72b5b10df41666103177c0267e6ba5cbaba4b7da
songyuanpeng <songyuanpeng@aixuexi.com>
2020-12-14 17:48:27 +0800

    feat> 并发关键字

---
+++ src/main/java/com/song/threads/CountDownLatchT.java
@@ -0,0 +1,67 @@
+package com.song.threads;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @ClassName CountDownLatchT
+ * @Description Valar Morghulis.Valar Dohaeris
+ * @Author songyp
+ * @Date 2020-12-14 16:42
+ */
+public class CountDownLatchT {
+
+
+    private static ExecutorService executors = Executors.newFixedThreadPool(5);
+    private static ExecutorService service = Executors.newFixedThreadPool(5);
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        final CountDownLatch count = new CountDownLatch(5);
+        final CountDownLatch latch = new CountDownLatch(5);
+        final CountDownLatch open = new CountDownLatch(1);
+
+        int i = 0;
+
+        while (i < 5) {
+            executors.submit(() -> {
+                try {
+                    open.await();
+                    System.out.println(Thread.currentThread().getName() + "号贵宾上门");
+                    count.countDown();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+            service.submit(() -> {
+                try {
+                    open.await();
+                    System.out.println(Thread.currentThread().getName() + "号贵宾的技师准备好了");
+                    latch.countDown();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            i++;
+
+
+        }
+        System.out.println("开门了");
+        open.countDown();
+        latch.await();
+        System.out.println("技师开始服务");
+        count.await();
+        System.out.println("贵宾到齐了，开始上菜");
+
+        System.out.println("all end");
+        executors.shutdown();
+        service.shutdown();
+    }
+
+
+}
---
+++ src/main/java/com/song/threads/CyclicBarrierT.java
@@ -0,0 +1,72 @@
+package com.song.threads;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @ClassName CyclicBarrierT
+ * @Description Valar Morghulis.Valar Dohaeris
+ * @Author songyp
+ * @Date 2020-12-14 17:36
+ */
+public class CyclicBarrierT {
+
+private static ExecutorService executorService = Executors.newFixedThreadPool(5);
+private static ExecutorService service = Executors.newFixedThreadPool(5);
+
+    public static void main(String[] args) {
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+
+        for (int i = 0;i<5;i++){
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName()+"学员准备好了");
+                    try {
+                        cyclicBarrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+"学生到le");
+
+                }
+            });
+        }
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0;i<5;i++){
+            service.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName()+"老师准备好了");
+                    try {
+                        cyclicBarrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+"老师到了");
+
+                }
+            });
+        }
+
+//        System.out.println("老师到齐了");
+        System.out.println("开始开会");
+
+        service.shutdown();
+        executorService.shutdown();
+    }
+}
