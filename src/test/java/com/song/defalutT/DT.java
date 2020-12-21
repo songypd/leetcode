3a60838daf1933d394b595ab9210050dfb5adea1
songyuanpeng <songypd@aliyun.com>
2020-12-21 23:31:15 +0800

    feat>optional的涨见识的操作

--- src/test/java/com/song/defalutT/DT.java
+++ src/test/java/com/song/defalutT/DT.java
@@ -1,10 +1,20 @@
 package com.song.defalutT;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Optional;
 
 public class DT {
     @Test
-    public void Test1(){
+    public void Test1() {
+        Optional demo = Optional.empty();
+        Optional alseEmpty = Optional.ofNullable(null);
+//Return {@code true} if there is a value present, otherwise {@code false}.
+        Assert.assertFalse(demo.isPresent());
 
+        Assert.assertFalse(alseEmpty.isPresent());
+
+        Assert.assertEquals("c",demo.orElseGet(()->"c"));
     }
 }
