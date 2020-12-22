d9d97f33951f23dac0ca9225797266c9d2af7a75
songyuanpeng <songypd@aliyun.com>
2020-12-22 23:10:13 +0800

    feat>stream的涨见识的操作

--- src/test/java/com/song/defalutT/DT.java
+++ src/test/java/com/song/defalutT/DT.java
@@ -1,9 +1,15 @@
 package com.song.defalutT;
 
+import lombok.Data;
+import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 public class DT {
     @Test
@@ -15,6 +21,35 @@ public class DT {
 
         Assert.assertFalse(alseEmpty.isPresent());
 
-        Assert.assertEquals("c",demo.orElseGet(()->"c"));
+        Assert.assertEquals("c", demo.orElseGet(() -> "c"));
+
+        List l1 = new ArrayList();
+//        l1.stream().collect(Collectors.toCollection(TreeSet::new));
+        l1.stream().collect(toCollection(TreeSet::new));
+
+
+        List<clazz> clazzes = Lists.newArrayList();
+        //按照是否开班进行区分
+        Map<Boolean, List<clazz>> collect = clazzes.stream().collect(partitioningBy(clazz::getOpen));
+        Map<Boolean, List<clazz>> collect1 = clazzes.stream().collect(groupingBy(clazz::getOpen));
+        //分组并将每组的个数输出
+        Map<Integer, Long> collect2 = clazzes.stream().collect(groupingBy(clazz::getCode, counting()));
+        Map<Integer, List<field>> collect3 = clazzes.stream().collect(groupingBy(clazz::getCode, mapping(clazz::getField, toList())));
+        String nameAdd = clazzes.stream().map(clazz::getName).collect(joining(",", "[", "]"));
+
     }
+}
+
+@Data
+class clazz {
+    private String name;
+    private Integer code;
+    private Boolean open;
+    private field field;
+
+}
+
+@Data
+class field {
+    private String name;
 }
