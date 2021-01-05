7124c4a467486bdf0af7fe4b24d91334fff669e4
songyuanpeng <songyuanpeng@aixuexi.com>
2021-01-05 19:51:49 +0800

    feat> 算法提交

---
+++ src/main/java/com/song/leetcode/answer/Answer102.java
@@ -0,0 +1,63 @@
+package com.song.leetcode.answer;
+
+import java.util.List;
+
+/**
+ * @ClassName Answer102
+ * @Description Valar Morghulis.Valar Dohaeris
+ * @Author songyp
+ * @Date 2021-01-05 16:11
+ * 二叉树的层序遍历
+ */
+public class Answer102 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+}
+/***
+ *给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ *  
+ *
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层序遍历结果：
+ *
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ *
+ *
+ *
+ *
+ * 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其自底向上的层序遍历为：
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
---
+++ src/main/java/com/song/threads/ReentranLockT.java
@@ -0,0 +1,75 @@
+package com.song.threads;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @ClassName ReentranLockT
+ * @Description Valar Morghulis.Valar Dohaeris
+ * @Author songyp
+ * @Date 2021-01-05 15:37
+ */
+public class ReentranLockT {
+
+
+
+    //此处可以考察公平锁与非公平锁的区别
+//    公平锁
+    /**
+     * Creates an instance of {@code ReentrantLock} with the
+     * given fairness policy.
+     *
+     * public ReentrantLock(boolean fair) {
+     *       sync = fair ? new FairSync() : new NonfairSync();
+     *     }
+     *
+     *
+     *公平锁-----》new ReentrantLock(true)
+     * 线程a获得到了锁
+     * 线程b获得到了锁
+     * 线程c获得到了锁
+     * 线程d获得到了锁
+     * 线程e获得到了锁
+     * 线程a获得到了锁
+     * 线程b获得到了锁
+     * 线程c获得到了锁
+     * 线程d获得到了锁
+     * 线程e获得到了锁
+     * 线程a获得到了锁
+     * 线程b获得到了锁
+     * 线程c获得到了锁
+     * 线程d获得到了锁
+     * 线程e获得到了锁
+     *
+     * 非公平所-----》new ReentrantLock(false);
+     *
+     * @param fair {@code true} if this lock should use a fair ordering policy
+     */
+//    private static final Lock lock = new ReentrantLock(true);
+    private static final Lock lock = new ReentrantLock(false);
+
+    public static void main(String[] args) {
+        new Thread(()->test(),"线程a").start();
+        new Thread(()->test(),"线程b").start();
+        new Thread(()->test(),"线程c").start();
+        new Thread(()->test(),"线程d").start();
+        new Thread(()->test(),"线程e").start();
+    }
+
+    public static void test(){
+
+        for (int i= 0;i < 3;i++){
+            try{
+                lock.lock();
+                System.out.println(Thread.currentThread().getName()+"获得到了锁");
+                Thread.sleep(100);
+//                TimeUnit.SECONDS.sleep(1);
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }finally {
+                lock.unlock();
+            }
+
+        }
+    }
+}
